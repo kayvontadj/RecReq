d4ec2c9acffc49ed39607322afab5b4e19d9d1eb
Kayvon Tadj <kayvontadj@gmail.com>
2022-12-26 13:03:55 -0800

    Initial Commit

---
+++ RecReqSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RecReqSwiftUI
+//
+//  Created by Persson Group on 12/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RecReqSwiftUI/RecReqSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecReqSwiftUIApp.swift
+//  RecReqSwiftUI
+//
+//  Created by Persson Group on 12/26/22.
+//
+
+import SwiftUI
+
+@main
+struct RecReqSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
